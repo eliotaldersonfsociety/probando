d39baefa0cde05fbcacc36758675a65c300e4bcf
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-03-08 00:06:00 -0500

    Update start.js

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 // start.js
-const { exec } = require('child_process');
+import { exec } from 'child_process';
 
 // Función para ejecutar comandos en la terminal
 function runCommand(command) {
@@ -20,16 +20,13 @@ function runCommand(command) {
 // Función principal
 async function start() {
   try {
-    console.log('Iniciando el backend...');
-    await runCommand('node app/api/index.js &'); // Ejecuta el backend en segundo plano
-
     console.log('Compilando el frontend...');
     await runCommand('npm run build'); // Compila el frontend
 
-    console.log('Iniciando el frontend en modo desarrollo...');
-    await runCommand('npm run dev'); // Inicia el frontend en modo desarrollo
+    console.log('Iniciando el backend...');
+    await runCommand('node app/api/index.js'); // Inicia el backend
 
-    console.log('Aplicación lista para desarrollo.');
+    console.log('Aplicación lista para producción.');
   } catch (error) {
     console.error('Error durante el inicio:', error);
     process.exit(1); // Termina el proceso con un código de error
