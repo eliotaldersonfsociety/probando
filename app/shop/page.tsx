f51f8a56957f165c60506d791b4b7728582ce078
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-03-07 19:08:26 -0500

    Update page.tsx

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -221,7 +221,9 @@ export default function ShopPage() {
               <CardDescription>{product.description}</CardDescription>
             </CardHeader>
             <CardFooter>
-              <Button onClick={() => addToCart(product)} leftIcon={<Plus />}>Add to Cart</Button>
+              <Button onClick={() => addToCart(product)}>
+                <Plus className="mr-2" /> Add to Cart
+              </Button>
             </CardFooter>
           </Card>
         ))}
@@ -233,12 +235,18 @@ export default function ShopPage() {
               <div key={item.id} className="cart-item">
                 <span>{item.name}</span>
                 <div className="quantity-controls">
-                  <Button onClick={() => updateQuantity(item.id, -1)} disabled={item.quantity <= 1}><Minus /></Button>
+                  <Button onClick={() => updateQuantity(item.id, -1)} disabled={item.quantity <= 1}>
+                    <Minus />
+                  </Button>
                   <span>{item.quantity}</span>
-                  <Button onClick={() => updateQuantity(item.id, 1)}><Plus /></Button>
+                  <Button onClick={() => updateQuantity(item.id, 1)}>
+                    <Plus />
+                  </Button>
                 </div>
                 <span>${item.price * item.quantity}</span>
-                <Button onClick={() => removeFromCart(item.id)}><Trash2 /></Button>
+                <Button onClick={() => removeFromCart(item.id)}>
+                  <Trash2 />
+                </Button>
               </div>
             ))}
           </div>
